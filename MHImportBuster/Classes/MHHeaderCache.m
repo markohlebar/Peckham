4d07ce9f6cd398c921b8ce9f797a30fb9cb3883b
Clement <ClementPadovani@users.noreply.github.com>
2015-07-30 13:18:09 +0200

    Removed import statement since it wasn't used anywhere.

--- MHImportBuster/Classes/MHHeaderCache.m
+++ MHImportBuster/Classes/MHHeaderCache.m
@@ -19,7 +19,6 @@
 #import "XCBuildSettings.h"
 #import "XCProject+Extensions.h"
 #import "XCProject+SubProject.h"
-#import <XcodeEditor/XCProjectBuildConfig.h>
 #import "NSObject+MHLogMethods.h"
 #import "MHSourceFile.h"
 #import "MHConcreteSourceFile.h"
