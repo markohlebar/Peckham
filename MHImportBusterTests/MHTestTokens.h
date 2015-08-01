fb096c8baa46adf61884b0a939f4728a98e44be5
Clement <ClementPadovani@users.noreply.github.com>
2015-08-01 19:35:34 +0200

    Renamed class methods used.
    `PKToken` no longer implements `+ [PKToken tokenWithTokenType:stringValue:floatValue:]` but changed to `+ [PKToken tokenWithTokenType:stringValue:doubleValue:]`.

--- MHImportBusterTests/MHTestTokens.h
+++ MHImportBusterTests/MHTestTokens.h
@@ -34,103 +34,103 @@ static void (^tokenFeedBlock)(MHStatement *loc, NSArray* tokens) = ^(MHStatement
 
 static NSArray* (^frameworkTokensWithStrings) (NSString*, NSString*) = ^NSArray* (NSString *subpath, NSString *header) {
     return @[
-             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:@"#import" floatValue:0],
-             [PKToken tokenWithTokenType:PKTokenTypeSymbol stringValue:@"<" floatValue:0],
-             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:subpath floatValue:0],
-             [PKToken tokenWithTokenType:PKTokenTypeSymbol stringValue:@"/" floatValue:0],
-             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:header floatValue:0],
-             [PKToken tokenWithTokenType:PKTokenTypeSymbol stringValue:@"." floatValue:0],
-             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:@"h" floatValue:0],
-             [PKToken tokenWithTokenType:PKTokenTypeSymbol stringValue:@">" floatValue:0],
-             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:@"\n" floatValue:0],
+             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:@"#import" doubleValue:0],
+             [PKToken tokenWithTokenType:PKTokenTypeSymbol stringValue:@"<" doubleValue:0],
+             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:subpath doubleValue:0],
+             [PKToken tokenWithTokenType:PKTokenTypeSymbol stringValue:@"/" doubleValue:0],
+             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:header doubleValue:0],
+             [PKToken tokenWithTokenType:PKTokenTypeSymbol stringValue:@"." doubleValue:0],
+             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:@"h" doubleValue:0],
+             [PKToken tokenWithTokenType:PKTokenTypeSymbol stringValue:@">" doubleValue:0],
+             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:@"\n" doubleValue:0],
              ];
 };
 
 static NSArray* (^projectTokensWithStrings) (NSString*, NSString*) = ^NSArray* (NSString *subpath, NSString *header) {
     return @[
-             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:@"#import" floatValue:0],
-             [PKToken tokenWithTokenType:PKTokenTypeSymbol stringValue:@"\"" floatValue:0],
-             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:subpath floatValue:0],
-             [PKToken tokenWithTokenType:PKTokenTypeSymbol stringValue:@"/" floatValue:0],
-             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:header floatValue:0],
-             [PKToken tokenWithTokenType:PKTokenTypeSymbol stringValue:@"." floatValue:0],
-             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:@"h" floatValue:0],
-             [PKToken tokenWithTokenType:PKTokenTypeSymbol stringValue:@"\"" floatValue:0],
-             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:@"\n" floatValue:0],
+             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:@"#import" doubleValue:0],
+             [PKToken tokenWithTokenType:PKTokenTypeSymbol stringValue:@"\"" doubleValue:0],
+             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:subpath doubleValue:0],
+             [PKToken tokenWithTokenType:PKTokenTypeSymbol stringValue:@"/" doubleValue:0],
+             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:header doubleValue:0],
+             [PKToken tokenWithTokenType:PKTokenTypeSymbol stringValue:@"." doubleValue:0],
+             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:@"h" doubleValue:0],
+             [PKToken tokenWithTokenType:PKTokenTypeSymbol stringValue:@"\"" doubleValue:0],
+             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:@"\n" doubleValue:0],
              ];
 };
 
 static NSArray* (^projectTokensWithString) (NSString*) = ^NSArray* (NSString *header) {
     return @[
-             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:@"#import" floatValue:0],
-             [PKToken tokenWithTokenType:PKTokenTypeSymbol stringValue:@"\"" floatValue:0],
-             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:header floatValue:0],
-             [PKToken tokenWithTokenType:PKTokenTypeSymbol stringValue:@"." floatValue:0],
-             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:@"h" floatValue:0],
-             [PKToken tokenWithTokenType:PKTokenTypeSymbol stringValue:@"\"" floatValue:0],
-             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:@"\n" floatValue:0],
+             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:@"#import" doubleValue:0],
+             [PKToken tokenWithTokenType:PKTokenTypeSymbol stringValue:@"\"" doubleValue:0],
+             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:header doubleValue:0],
+             [PKToken tokenWithTokenType:PKTokenTypeSymbol stringValue:@"." doubleValue:0],
+             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:@"h" doubleValue:0],
+             [PKToken tokenWithTokenType:PKTokenTypeSymbol stringValue:@"\"" doubleValue:0],
+             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:@"\n" doubleValue:0],
              ];
 };
 
 static NSArray* (^frameworkTokens) () = ^NSArray* () {
     return @[
-             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:@"#import" floatValue:0],
-             [PKToken tokenWithTokenType:PKTokenTypeSymbol stringValue:@"<" floatValue:0],
-             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:@"Framework" floatValue:0],
-             [PKToken tokenWithTokenType:PKTokenTypeSymbol stringValue:@"/" floatValue:0],
-             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:@"Header" floatValue:0],
-             [PKToken tokenWithTokenType:PKTokenTypeSymbol stringValue:@"." floatValue:0],
-             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:@"h" floatValue:0],
-             [PKToken tokenWithTokenType:PKTokenTypeSymbol stringValue:@">" floatValue:0],
-             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:@"\n" floatValue:0],
+             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:@"#import" doubleValue:0],
+             [PKToken tokenWithTokenType:PKTokenTypeSymbol stringValue:@"<" doubleValue:0],
+             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:@"Framework" doubleValue:0],
+             [PKToken tokenWithTokenType:PKTokenTypeSymbol stringValue:@"/" doubleValue:0],
+             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:@"Header" doubleValue:0],
+             [PKToken tokenWithTokenType:PKTokenTypeSymbol stringValue:@"." doubleValue:0],
+             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:@"h" doubleValue:0],
+             [PKToken tokenWithTokenType:PKTokenTypeSymbol stringValue:@">" doubleValue:0],
+             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:@"\n" doubleValue:0],
              ];
 };
 
 static NSArray* (^projectTokens) () = ^NSArray* () {
     return @[
-             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:@"#import" floatValue:0],
-             [PKToken tokenWithTokenType:PKTokenTypeSymbol stringValue:@"\"" floatValue:0],
-             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:@"Subpath" floatValue:0],
-             [PKToken tokenWithTokenType:PKTokenTypeSymbol stringValue:@"/" floatValue:0],
-             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:@"Header" floatValue:0],
-             [PKToken tokenWithTokenType:PKTokenTypeSymbol stringValue:@"." floatValue:0],
-             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:@"h" floatValue:0],
-             [PKToken tokenWithTokenType:PKTokenTypeSymbol stringValue:@"\"" floatValue:0],
-             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:@"\n" floatValue:0],
+             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:@"#import" doubleValue:0],
+             [PKToken tokenWithTokenType:PKTokenTypeSymbol stringValue:@"\"" doubleValue:0],
+             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:@"Subpath" doubleValue:0],
+             [PKToken tokenWithTokenType:PKTokenTypeSymbol stringValue:@"/" doubleValue:0],
+             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:@"Header" doubleValue:0],
+             [PKToken tokenWithTokenType:PKTokenTypeSymbol stringValue:@"." doubleValue:0],
+             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:@"h" doubleValue:0],
+             [PKToken tokenWithTokenType:PKTokenTypeSymbol stringValue:@"\"" doubleValue:0],
+             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:@"\n" doubleValue:0],
              ];
 };
 
 static NSArray* (^projectTokensNoSubpath) () = ^NSArray* () {
     return @[
-             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:@"#import" floatValue:0],
-             [PKToken tokenWithTokenType:PKTokenTypeSymbol stringValue:@"\"" floatValue:0],
-             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:@"Header" floatValue:0],
-             [PKToken tokenWithTokenType:PKTokenTypeSymbol stringValue:@"." floatValue:0],
-             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:@"h" floatValue:0],
-             [PKToken tokenWithTokenType:PKTokenTypeSymbol stringValue:@"\"" floatValue:0],
-             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:@"\n" floatValue:0],
+             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:@"#import" doubleValue:0],
+             [PKToken tokenWithTokenType:PKTokenTypeSymbol stringValue:@"\"" doubleValue:0],
+             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:@"Header" doubleValue:0],
+             [PKToken tokenWithTokenType:PKTokenTypeSymbol stringValue:@"." doubleValue:0],
+             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:@"h" doubleValue:0],
+             [PKToken tokenWithTokenType:PKTokenTypeSymbol stringValue:@"\"" doubleValue:0],
+             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:@"\n" doubleValue:0],
              ];
 };
 
 static NSArray* (^classMethodTokens) () = ^NSArray* () {
     return @[
-             [PKToken tokenWithTokenType:PKTokenTypeSymbol stringValue:@"+" floatValue:0],
-             [PKToken tokenWithTokenType:PKTokenTypeSymbol stringValue:@"(" floatValue:0],
-             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:@"void" floatValue:0],
-             [PKToken tokenWithTokenType:PKTokenTypeSymbol stringValue:@")" floatValue:0],
-             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:@"classMethod" floatValue:0],
-             [PKToken tokenWithTokenType:PKTokenTypeSymbol stringValue:@"{" floatValue:0]
+             [PKToken tokenWithTokenType:PKTokenTypeSymbol stringValue:@"+" doubleValue:0],
+             [PKToken tokenWithTokenType:PKTokenTypeSymbol stringValue:@"(" doubleValue:0],
+             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:@"void" doubleValue:0],
+             [PKToken tokenWithTokenType:PKTokenTypeSymbol stringValue:@")" doubleValue:0],
+             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:@"classMethod" doubleValue:0],
+             [PKToken tokenWithTokenType:PKTokenTypeSymbol stringValue:@"{" doubleValue:0]
              ];
 };
 
 static NSArray* (^instanceMethodTokens) () = ^NSArray* () {
     return @[
-             [PKToken tokenWithTokenType:PKTokenTypeSymbol stringValue:@"-" floatValue:0],
-             [PKToken tokenWithTokenType:PKTokenTypeSymbol stringValue:@"(" floatValue:0],
-             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:@"void" floatValue:0],
-             [PKToken tokenWithTokenType:PKTokenTypeSymbol stringValue:@")" floatValue:0],
-             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:@"instanceMethod" floatValue:0],
-             [PKToken tokenWithTokenType:PKTokenTypeSymbol stringValue:@"{" floatValue:0]
+             [PKToken tokenWithTokenType:PKTokenTypeSymbol stringValue:@"-" doubleValue:0],
+             [PKToken tokenWithTokenType:PKTokenTypeSymbol stringValue:@"(" doubleValue:0],
+             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:@"void" doubleValue:0],
+             [PKToken tokenWithTokenType:PKTokenTypeSymbol stringValue:@")" doubleValue:0],
+             [PKToken tokenWithTokenType:PKTokenTypeWord stringValue:@"instanceMethod" doubleValue:0],
+             [PKToken tokenWithTokenType:PKTokenTypeSymbol stringValue:@"{" doubleValue:0]
              ];
 };
 
