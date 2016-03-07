bb87512b88d0ce0bc321920fc386dca3f7d2375b
Clément Padovani <ClementPadovani@users.noreply.github.com>
2016-03-07 14:27:29 +0100

    Seriously, how long does this have to be?
    Signed-off-by: Clément Padovani <ClementPadovani@users.noreply.github.com>

--- MHImportBusterTests/Controllers/MHSourceFileSearchControllerSpec.m
+++ MHImportBusterTests/Controllers/MHSourceFileSearchControllerSpec.m
@@ -107,7 +107,7 @@ describe(@"MHSourceFileSearchController", ^{
 
 		[[searchController.searchString should] equal:@""];
 		[[searchController.filteredSourceFiles should] equal:sourceFiles];
-		[[expectFutureValue(searchArray) shouldEventuallyBeforeTimingOutAfter(60)] beNil];
+		[[expectFutureValue(searchArray) shouldEventuallyBeforeTimingOutAfter(60 * 2)] beNil];
 
 	});
 });
