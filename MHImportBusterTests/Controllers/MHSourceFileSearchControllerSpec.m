919260a0db92da66b55c528e8811a777a2284003
Dominic Jodoin <dominic@travis-ci.com>
2016-03-11 21:20:20 -0500

    More logs

--- MHImportBusterTests/Controllers/MHSourceFileSearchControllerSpec.m
+++ MHImportBusterTests/Controllers/MHSourceFileSearchControllerSpec.m
@@ -100,6 +100,7 @@ describe(@"MHSourceFileSearchController", ^{
 		__block NSArray *searchArray = nil;
 		[searchController search:@"importB"
 				   searchBlock:^(NSArray *array) {
+				           MHLog("TRAVISCI setting search array");
 					   searchArray = array;
 				   }];
 
