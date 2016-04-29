84618471d64162c0934ae2ba444ce6474eae9fe9
Clément Padovani <ClementPadovani@users.noreply.github.com>
2016-04-29 21:34:05 +0200

    Added fuzzy search tests.

--- MHImportBusterTests/NSString+FuzzySearchSpec.m
+++ MHImportBusterTests/NSString+FuzzySearchSpec.m
@@ -14,6 +14,43 @@ SPEC_BEGIN(NSString_FuzzySearchSpec)
 
 describe(@"NSString+FuzzySearch", ^{
 
+    __block NSString *searchStringWithSingleWord = nil;
+
+    __block NSString *searchStringWithTwoWords = nil;
+
+    beforeAll(^{
+
+        NSString *_searchStringWithSingleWord = @"helloWorld";
+
+        searchStringWithSingleWord = [_searchStringWithSingleWord mh_fuzzifiedSearchString];
+
+        NSString *_searchStringWithTwoWords = @"hello world";
+
+        searchStringWithTwoWords = [_searchStringWithTwoWords mh_fuzzifiedSearchString];
+
+    });
+
+    it(@"should not have empty search strings", ^{
+        [[searchStringWithSingleWord shouldNot] beNil];
+
+        [[searchStringWithSingleWord should] beKindOfClass: [NSString class]];
+
+        [[searchStringWithSingleWord shouldNot] beEmpty];
+
+        [[searchStringWithTwoWords shouldNot] beNil];
+
+        [[searchStringWithTwoWords should] beKindOfClass: [NSString class]];
+
+        [[searchStringWithTwoWords shouldNot] beEmpty];
+    });
+
+    it(@"should be equal to fuzzy search strings", ^{
+
+        [[searchStringWithSingleWord should] equal: @"*h*e*l*l*o*W*o*r*l*d*"];
+
+        [[searchStringWithTwoWords should] equal: @"*h*e*l*l*o* *w*o*r*l*d*"];
+
+    });
 });
 
 SPEC_END
