afd811331503a80d930efe26cc4c5ea536c21aa8
Clément Padovani <ClementPadovani@users.noreply.github.com>
2016-04-29 21:24:58 +0200

    Created test file for fuzzy search.

---
+++ MHImportBusterTests/NSString+FuzzySearchSpec.m
@@ -0,0 +1,19 @@
+//
+//  NSString+FuzzySearchSpec.m
+//  MHImportBuster
+//
+//  Created by Clément Padovani on 4/29/16.
+//  Copyright 2016 Marko Hlebar. All rights reserved.
+//
+
+#import <Kiwi/Kiwi.h>
+#import "NSString+FuzzySearch.h"
+
+
+SPEC_BEGIN(NSString_FuzzySearchSpec)
+
+describe(@"NSString+FuzzySearch", ^{
+
+});
+
+SPEC_END
