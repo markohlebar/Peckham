a151b2dffb521bc3640d54564000db87e0f735f4
Marko Hlebar <marko.hlebar@gmail.com>
2016-05-02 15:11:31 +0100

    Removed sorting logic from the search operation.

--- MHImportBuster/Classes/MHSearchArrayOperation.m
+++ MHImportBuster/Classes/MHSearchArrayOperation.m
@@ -10,8 +10,6 @@
 #import "NSString+Extensions.h"
 #import "MHSourceFile.h"
 #import "NSString+FuzzySearch.h"
-#import "NSString+CamelCase.h"
-#import "MHHeaderCache.h"
 
 @implementation MHSearchArrayOperation
 + (instancetype) operationWithSearchArray:(NSArray *) searchArray
@@ -38,45 +36,7 @@
 - (void) execute {
     NSPredicate *predicate = [NSPredicate predicateWithFormat:@"lastPathComponent LIKE[cd] %@", [self.searchString mh_fuzzifiedSearchString]];
     NSArray *results = [self.searchArray filteredArrayUsingPredicate:predicate];
-    results = [self resultsSortedByCapitalizedResultsMatches:results];
-    results = [self resultsSortedByFrameworksMatches:results];
     [self notifyWithResults:results];
-}
-
-- (NSArray *)resultsSortedByCapitalizedResultsMatches:(NSArray *)results {
-    NSArray *sorted = [results sortedArrayUsingComparator:^NSComparisonResult(id <MHSourceFile> _Nonnull obj1, id <MHSourceFile> _Nonnull obj2) {
-        BOOL matchCamelCase1 = [obj1.name.mh_camelCaseInitials.lowercaseString containsString:_searchString];
-        BOOL matchCamelCase2 = [obj2.name.mh_camelCaseInitials.lowercaseString containsString:_searchString];
-        
-        if (matchCamelCase1 && matchCamelCase2) {
-            return NSOrderedSame;
-        }
-        else if (matchCamelCase2) {
-            return NSOrderedDescending;
-        }
-        
-        return NSOrderedAscending;
-    }];
-    return sorted;
-}
-
-- (NSArray *)resultsSortedByFrameworksMatches:(NSArray *)results {
-    MHHeaderCache *cache = [MHHeaderCache sharedCache];
-    
-    NSArray *sorted = [results sortedArrayUsingComparator:^NSComparisonResult(id <MHSourceFile> _Nonnull obj1, id <MHSourceFile> _Nonnull obj2) {
-        BOOL isFramework1 = [cache isFrameworkHeader:obj1];
-        BOOL isFramework2 = [cache isFrameworkHeader:obj2];
-        
-        if (isFramework1 && isFramework2) {
-            return NSOrderedSame;
-        }
-        else if (isFramework1) {
-            return NSOrderedDescending;
-        }
-        
-        return NSOrderedAscending;
-    }];
-    return sorted;
 }
 
 - (void) notifyWithResults:(NSArray *)results {
